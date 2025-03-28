47f8a9a90a32b6122132aeaf43b7a2ef8cc5fcae
jsaloian755 <aloianjs@appstate.edu>
2025-03-28 12:26:24 -0400

    Fixed Dollar.java (again)

--- src/main/Dollar.java
+++ src/main/Dollar.java
@@ -5,8 +5,8 @@ public class Dollar extends Coin
     public Dollar()
     {
         super("Dollar", DOLLAR_VALUE, "IN GOD WE TRUST", "E PLURIBUS UNUM", 
-                  "LIBERTY", "UNITED STATES OF AMERICA", "S_Anthony", "backImage", 
-                  "ONE DOLLAR", true, "", Calendar.getInstance().get(Calendar.YEAR));
+                  "LIBERTY", "UNITED STATES OF AMERICA", "S_Anthony", "Moon_Eagle", 
+                  "ONE DOLLAR", true, "Cupro-Nickel", Calendar.getInstance().get(Calendar.YEAR));
     }
 
     public Dollar(int year)
