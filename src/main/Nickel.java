61b3fd54d0d17aafda12bbcee3022f783e46c6c3
jsaloian755 <aloianjs@appstate.edu>
2025-04-06 23:33:55 -0400

    Updated Nickel

--- src/main/Nickel.java
+++ src/main/Nickel.java
@@ -1,18 +1,18 @@
 import java.util.Calendar;
 
-public class nickel extends Coin
+public class Nickel extends Coin
 {
     public Nickel()
     {
         super("Nickel", NICKEL_VALUE, "IN GOD WE TRUST", "E PLURIBUS UNUM", 
                   "LIBERTY", "UNITED STATES OF AMERICA", "T_Jefferson", "Jefferson_Memorial", 
-                  "FIVE CENTS", false, "Cupro-Nickel", Calendar.getInstance().get(Calendar.YEAR));
+                  "FIVE CENTS", false, new CuproNickel(), Calendar.getInstance().get(Calendar.YEAR));
     }
 
     public Nickel(int year)
     {
         super("Nickel", NICKEL_VALUE, "IN GOD WE TRUST", "E PLURIBUS UNUM", 
         "LIBERTY", "UNITED STATES OF AMERICA", "T_Jefferson", "Jefferson_Memorial", 
-        "FIVE CENTS", false, "Cupro-Nickel", year);
+        "FIVE CENTS", false, new CuproNickel(), year);
     }
 }
