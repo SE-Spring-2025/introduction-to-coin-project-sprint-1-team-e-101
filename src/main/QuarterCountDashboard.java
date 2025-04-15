61b3fa8fe34d355ba3c907bd04b5cf9e0300d23e
jsaloian755 <aloianjs@appstate.edu>
2025-04-14 23:09:50 -0400

    Gave dashboard classes a shot (feel free to modify)

---
+++ src/main/QuarterCountDashboard.java
@@ -0,0 +1,25 @@
+public class QuarterCountDashboard extends JFrame implements Observer
+{
+    private JLabel label;
+
+    public QuarterCountDashboard(CoinCounts coinCounts)
+    {
+        coinCounts.addObserver(this);
+
+        setTitle("Quarter Count Dashboard");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        label = new JLabel("Total Quarters: " + coinCounts.getTotalQuarters());
+        add(label);
+
+        pack();
+        setVisible(true);
+    }
+
+    @Override
+    public void update()
+    {
+        label.setText("Total Quarters: " + coinCounts.getTotalQuarters());
+    }
+}
---
+++ src/main/TotalCoinsDashboard.java
@@ -0,0 +1,25 @@
+public class TotalCoinsDashboard extends JFrame implements Observer
+{
+    private JLabel label;
+
+    public TotalCoinsDashboard(CoinCounts coinCounts)
+    {
+        coinCounts.addObserver(this);
+
+        setTitle("Total Coins Dashboard");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        label = new JLabel("Total Coins: " + coinCounts.getTotalCoins());
+        add(label);
+
+        pack();
+        setVisible(true);
+    }
+
+    @Override
+    public void update()
+    {
+        label.setText("Total Coins: " + coinCounts.getTotalCoins());
+    }
+}
