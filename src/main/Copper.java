d4d84171f2c91dd81065a7fa24d4038b21a38b67
John Kearney <John@kearneyjohn.com>
2025-04-21 10:01:14 -0400

    Clean S-3
    Wipe files for clean refactor

--- src/main/Copper.java
+++
@@ -1,8 +0,0 @@
-public class Copper implements Metallurgy
-{
-    @Override
-    public String smelt()
-    {
-        return "Copper";
-    }
-}
--- src/main/CuproNickel.java
+++
@@ -1,8 +0,0 @@
-public class CuproNickel implements Metallurgy
-{
-    @Override
-    public String smelt()
-    {
-        return "Cupro-Nickel";
-    }
-}
