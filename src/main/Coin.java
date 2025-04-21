dc32a0e7037ae1bdf54045df29f547408d1d5cd4
John Kearney <John@kearneyjohn.com>
2025-04-21 09:16:52 -0400

    Bring up to date with main & s1

--- src/main/Coin.java
+++ src/main/Coin.java
@@ -1,33 +1,108 @@
+import java.util.Calendar;
 import java.text.DecimalFormat;
 
-public abstract class Coin {
-    public static final double PENNY_VALUE = 0.01;
-    public static final double NICKEL_VALUE = 0.05;
-    public static final double DIME_VALUE = 0.10;
-    public static final double QUARTER_VALUE = 0.25;
-    public static final double HALFDOLLAR_VALUE = 0.50;
-    public static final double DOLLAR_VALUE = 1.00;
+public class Coin 
+{
+	private double value;
+	private String commonName;
+	private String frontMotto;
+	private String backMotto;
+	private String frontLabel;
+	private String backLabel;
+	private String metallurgy;
+	private boolean ridgedEdge;
+	
+	public static final double PENNY_VALUE = 0.01;
+	public static final double NICKEL_VALUE = 0.05;
+	public static final double DIME_VALUE = 0.10;
+	public static final double QUARTER_VALUE = 0.25;
+	public static final double HALFDOLLAR_VALUE = 0.50;
+	public static final double DOLLAR_VALUE = 1.00;
+	private String familiarName;
+	private String frontImage;
+	private String backImage;
+	private String valueDescription;
 
-    // Instance variables defining various properties of a coin.
-    private String familiarName;
-    private double value;
-    private String frontMotto;
-    private String backMotto;
-    private String frontLabel;
-    private String backLabel;
-    private String frontImage;
-    private String backImage;
-    private String valueDescription;
-    private boolean ridgedEdge;
-    private String metallurgy;
-    private int manufactureYear;
-    
-    public Coin(String familiarName, double value, String frontMotto, String backMotto, String frontLabel, 
-		String backLabel, String frontImage, String backImage, String valueDescription, boolean ridgedEdge, 
-		String metallurgy, int manufactureYear)
+	
+	private int manufactureYear;
+	
+	public Coin() 
 	{
-		this.familiarName = familiarName;
+		this(0);
+	}
+	
+	public Coin(double value) 
+	{
+		this(value, (Calendar.getInstance()).get(Calendar.YEAR));
+	}
+	
+	public Coin(double value, int year) 
+	{
+	
+		if (cmpDoubles(value, PENNY_VALUE)) 
+		{
+			familiarName = "Penny";
+			frontImage = "A_Lincoln";
+			backImage = "Lincoln_Memorial";
+			valueDescription = "ONE CENT";
+			ridgedEdge = false;
+			metallurgy = "Copper";
+		}
+	
+		else if (cmpDoubles(value, NICKEL_VALUE)) 
+		{
+			familiarName = "Nickel";
+			frontImage = "T_Jefferson";
+			backImage = "Jefferson_Memorial";
+			valueDescription = "FIVE CENTS";
+			ridgedEdge = false;
+			metallurgy = "Cupro-Nickel";
+		}
+	
+		else if (cmpDoubles(value, DIME_VALUE)) 
+		{
+			familiarName = "Dime";
+			frontImage = "F_Roosevelt";
+			backImage = "Torch_Branches";
+			valueDescription = "ONE DIME";
+			ridgedEdge = true;
+			metallurgy = "Cupro-Nickel";
+		}
+	
+		else if (cmpDoubles(value, QUARTER_VALUE)) 
+		{
+			familiarName = "Quarter";
+			frontImage = "G_Washington";
+			backImage = "Eagle";
+			valueDescription = "QUARTER DOLLAR";
+			ridgedEdge = true;
+			metallurgy = "Cupro-Nickel";
+		}
+	
+		else if (cmpDoubles(value, HALFDOLLAR_VALUE)) 
+		{
+			familiarName = "HalfDollar";
+			frontImage = "J_Kennedy";
+			backImage = "Presidential_Seal";
+			valueDescription = "HALF DOLLAR";
+			ridgedEdge = true;
+			metallurgy = "Cupro-Nickel";
+		}
+	
+		else if (cmpDoubles(value, DOLLAR_VALUE)) 
+		{
+			familiarName = "Dollar";
+			frontImage = "S_Anthony";
+			backImage = "Moon_Eagle";
+			valueDescription = "ONE DOLLAR";
+			ridgedEdge = true;
+			metallurgy = "Cupro-Nickel";
+		}
+	
+		else value = 0;
+	
 		this.value = value;
+<<<<<<< Updated upstream
 		this.frontMotto = frontMotto;
 		this.backMotto = backMotto;
 		this.frontLabel = frontLabel;
@@ -254,6 +329,8 @@ public class Coin
 		else value = 0;
 	
 		this.value = value;
+=======
+>>>>>>> Stashed changes
 		this.manufactureYear = year;
 	
 		frontMotto = "IN GOD WE TRUST";
